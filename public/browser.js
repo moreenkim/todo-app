eefbe10eff39ae7f3d31e6616c16f5511b96e1ed
moreenkim <moreenkim36@gmail.com>
2020-07-22 09:26:08 +0300

    Update an existing item in the DB

--- public/browser.js
+++ public/browser.js
@@ -1,13 +1,23 @@
 document.addEventListener('click', function (e) {
   if (e.target.classList.contains('edit-me')) {
-    let userInput = prompt('enter the new text');
-    axios
-      .post('/update-item', { text: userInput })
-      .then(function () {
-        //do something herre
-      })
-      .catch(function () {
-        console.log('try again later');
-      });
+    let userInput = prompt(
+      'enter the new text',
+      e.target.parentElement.parentElement.querySelector('.item-text').innerHTML
+    );
+    if (userInput) {
+      axios
+        .post('/update-item', {
+          text: userInput,
+          id: e.target.getAttribute('data-id'),
+        })
+        .then(function () {
+          e.target.parentElement.parentElement.querySelector(
+            '.item-text'
+          ).innerHTML = userInput;
+        })
+        .catch(function () {
+          console.log('try again later');
+        });
+    }
   }
 });
--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.get('/', function (req, res) {
           return ` <li class="list-group-item list-group-item-action d-flex align-items-center justify-content-between">
           <span class="item-text">${item.text}</span>
           <div>
-            <button class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
+            <button data-id="${item._id}" class="edit-me btn btn-secondary btn-sm mr-1">Edit</button>
             <button class="delete-me btn btn-danger btn-sm">Delete</button>
           </div>
         </li>`;
@@ -76,6 +76,11 @@ app.post('/create-item', function (req, res) {
 });
 
 app.post('/update-item', function (req, res) {
-  console.log(req.body.text);
-  res.send('success');
+  db.collection('items').findOneAndUpdate(
+    { _id: new mongodb.ObjectId(req.body.id) },
+    { $set: { text: req.body.text } },
+    function () {
+      res.send('success');
+    }
+  );
 });
